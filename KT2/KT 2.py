80939a79bfa4ba6cad2cffaeca7cad5cc34cf532
YumanovS <61417604+YumanovS@users.noreply.github.com>
2020-05-11 09:20:14 +0300

    Update KT 2.py

--- KT2/KT 2.py
+++ KT2/KT 2.py
@@ -1,14 +1,7 @@
+import telebot
 import requests
 from bs4 import BeautifulSoup
 import time
-'''
-from telegram import Update
-from telegram.ext import Updater
-from telegram.ext import CallbackContext
-from telebot import apihelper
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
-'''
 
 
 URL = 'https://ria.ru/economy/'
@@ -16,83 +9,68 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 }
 
-
-
-#def
-
-
+last_name = 'none'
+'''
 def get_last_name(html):
     soup = BeautifulSoup(html, 'html.parser')
-    name = soup.find('a', class_ = 'list-item__title color-font-hover-only').get_text()
+    name = soup.find('a', class_='list-item__title color-font-hover-only').get_text()
     return name
+'''
 
 def get_html(url, params=None):
-    r = requests.get(url, headers = HEADERS,params = params)
+    r = requests.get(url, headers=HEADERS, params=params)
     return r
 
+
 def get_link(html):
-    global last_name 
+    global a
+    global last_name
     global URL
+    global a
+    global last_name
     soup = BeautifulSoup(html, 'html.parser')
-    name = soup.find('a', class_ = 'list-item__title color-font-hover-only').get_text()
-    link = soup.find('a', class_ = 'list-item__title color-font-hover-only').get('href')
-    time_post = soup.find('div',class_ = 'list-item__date').get_text(),
+    name = soup.find('a', class_='list-item__title color-font-hover-only').get_text()
+    link = soup.find('a', class_='list-item__title color-font-hover-only').get('href')
     html = get_html(link)
     if last_name != name:
         last_name = name
-        print(time_post)
         get_news(html.text)
     else:
-        print('нет новых новостей') #это для наглядности, в боте не нужно 
-    time.sleep(30)
-    html = get_html(URL) 
-    get_link(html.text)
-    
+       a = " "
+
+
 def get_news(html):
     global last_name
+    global a
     soup = BeautifulSoup(html, 'html.parser')
-    news = soup.find_all('div', class_ = 'article__text')
-    print(last_name)
+    news = soup.find_all('div', class_='article__text')
+    a = a + last_name
     for new in news:
-        print(new.get_text())
-       
-    
-def parse(): 
-    html = get_html(URL) 
-    if html.status_code == 200: 
+        a = a + new.get_text()
+
+bot = telebot.TeleBot(token='1169148923:AAGgN2JhEvTDCTOhMnmw4P6iDL2aU1nyRbY', threaded=False)
+
+
+
+a = ""
+
+@bot.message_handler(content_types=["text"])
+
+def get_text_messages(message):
+    global URL
+    global HEADERS
+    global a
+
+    html = get_html(URL)
+    if html.status_code == 200:
         get_link(html.text)
+    if a != ' ':
+        bot.send_message(message.from_user.id,a)
     else:
-        print('Error')
-     
-#main
-last_name = 'none'
-parse()
+        a = " "
+    time.sleep(20)
+    get_text_messages(message)
 
 
-'''
-TG_TOKEN = "1169148923:AAGgN2JhEvTDCTOhMnmw4P6iDL2aU1nyRbY"
-apihelper.proxy = {"https" : "socks5//128.199.208.93:32937"}
 
-def message_handler(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        text='test'
-    )
-
-def main():
-    print('Start')
-
-    updater = Updater(
-        token=TG_TOKEN,
-        url
-        use_context=True,
-    )
-
-    updater.dispatcher.add_handler(MessageHandler(filters=Filters.all, callback=message_handler))
-
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
-'''
+bot.polling(none_stop=True, interval=0)
